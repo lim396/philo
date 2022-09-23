b586900980d1575b6947934db2b9bfd5affd7ff7
lim <lbooks424@gmail.com>
2022-09-23 21:56:25 +0900

    [clean] some norm

--- main.c
+++ main.c
@@ -17,43 +17,97 @@
 #define MONITOR_ERROR 8
 #define PHILO_ERROR 16
 
-typedef struct s_config	t_config;
+typedef pthread_mutex_t		t_mutex;
+
+typedef struct s_config		t_config;
 struct s_config
 {
-	int	number_of_philosophers;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	num_of_must_eat; //too long must short
+	int		num_of_philo;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		num_of_must_eat;
 	long	start_time;
 };
 
-typedef struct s_overall_info	t_overall_info;
-struct s_overall_info
+typedef struct s_overall	t_overall;
+struct s_overall
 {
-	pthread_mutex_t	*fork;
-	pthread_mutex_t	*end_meals;
-	int	n_of_finish_eat;
-	pthread_mutex_t	*get_time_and_print;
-	pthread_mutex_t	*end_flag;
+	t_mutex	*fork;
+	t_mutex	*end_meals;
+	int		n_of_finish_eat;
+	t_mutex	*get_time_and_print;
+	t_mutex	*end_flag;
 	bool	end_simulation;
 };
 
-typedef struct s_philo	t_philo;
+typedef struct s_philo		t_philo;
 struct s_philo
 {
-	int id;
-	int	number_of_meals;
-	long	last_ate_time;
-	pthread_mutex_t	*last_ate;
-	pthread_t thread;
-	t_overall_info	*info;
-	t_config		*config;
+	int			id;
+	int			number_of_meals;
+	long		last_ate_time;
+	t_mutex		*last_ate;
+	pthread_t	thread;
+	t_overall	*info;
+	t_config	*config;
 };
 
 int	ft_isdigit(int c)
 {
 	return ('0' <= c && c <= '9');
+}
+
+int	ft_atoi(const char *str)
+{
+	long	integer;
+	int		sign;
+
+	integer = 0;
+	sign = 1;
+	while (('\t' <= *str && *str <= '\r') || *str == ' ')
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		sign = (44 - *str);
+		str++;
+	}
+	while ('0' <= *str && *str <= '9')
+	{
+		if (sign * integer > (LONG_MAX - (*str - '0')) / 10)
+			return ((int)LONG_MAX);
+		else if (sign * integer < (LONG_MIN + (*str - '0')) / 10)
+			return ((int)LONG_MIN);
+		integer = (integer * 10) + (*str - '0');
+		str++;
+	}
+	return (integer * sign);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	if (n == 0)
+		return ;
+	memset(s, '\0', n);
+	return ;
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*reserve_mem;
+
+	if (!(count && size))
+	{
+		count = 1;
+		size = 1;
+	}
+	if (count && (size > SIZE_MAX / count))
+		return (NULL);
+	reserve_mem = (void *)malloc(count * size);
+	if (!reserve_mem)
+		return (NULL);
+	ft_bzero(reserve_mem, count * size);
+	return (reserve_mem);
 }
 
 bool	is_natural_num_ex_zero(const char *str)
@@ -105,18 +159,18 @@ bool	check_args(int argc, char **argv)
 
 void	init_config(char **argv, t_config *config)
 {
-	config->number_of_philosophers = atoi(argv[1]);
-	config->time_to_die = atoi(argv[2]);
-	config->time_to_eat = atoi(argv[3]);
-	config->time_to_sleep = atoi(argv[4]);
+	config->num_of_philo = ft_atoi(argv[1]);
+	config->time_to_die = ft_atoi(argv[2]);
+	config->time_to_eat = ft_atoi(argv[3]);
+	config->time_to_sleep = ft_atoi(argv[4]);
 	config->num_of_must_eat = -1;
 	if (argv[5])
-		config->num_of_must_eat = atoi(argv[5]);
+		config->num_of_must_eat = ft_atoi(argv[5]);
 }
 
-void	malloc_error_handler(int n, t_overall_info *info, t_philo **philo)
+int	malloc_error_handler(int n, t_overall *info, t_philo **philo)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (philo != NULL)
@@ -133,49 +187,35 @@ void	malloc_error_handler(int n, t_overall_info *info, t_philo **philo)
 	free(info->get_time_and_print);
 	free(info->end_flag);
 	printf("malloc error\n");
+	return (1);
 }
 
-int	init_overall_info(int n, t_overall_info *info)
+int	init_overall_info(int n, t_overall *info)
 {
-//	int	i;
 	memset(info, 0, sizeof(*info));
 	info->n_of_finish_eat = 0;
 	info->end_simulation = 0;
-	info->end_meals = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
-	info->get_time_and_print = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
-	info->end_flag = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
-	info->fork = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * n);
+	info->end_meals = (t_mutex *)malloc(sizeof(t_mutex));
+	info->get_time_and_print = (t_mutex *)malloc(sizeof(t_mutex));
+	info->end_flag = (t_mutex *)malloc(sizeof(t_mutex));
+	info->fork = (t_mutex *)malloc(sizeof(t_mutex) * n);
 	if (!(info->end_meals) || !(info->get_time_and_print)
 		|| !(info->end_flag) || !(info->fork))
 	{
-		malloc_error_handler(0, info, NULL);
-		return (1);
+		return (malloc_error_handler(0, info, NULL));
 	}
 	return (0);
-//	pthread_mutex_init(info->end_meals, NULL);
-//	pthread_mutex_init(info->get_time_and_print, NULL);
-//	pthread_mutex_init(info->end_flag, NULL);
-//	i = 0;
-//	while (i < n)
-//	{
-//		pthread_mutex_init(&(info->fork)[i], NULL);
-//		i++;
-//	}
 }
 
-int	init_philosophers(t_config *config, t_overall_info *info, t_philo **philo)
+int	init_philosophers(t_config *config, t_overall *info, t_philo **philo)
 {
 	int	i;
 	int	n;
 
-	n = config->number_of_philosophers;
-//	*philo = (t_philo *)malloc(sizeof(t_philo) * n);
-	*philo = (t_philo *)calloc(sizeof(t_philo), n);
+	n = config->num_of_philo;
+	*philo = (t_philo *)ft_calloc(sizeof(t_philo), n);
 	if (*philo == NULL)
-	{
-		malloc_error_handler(0, info, philo);
-		return (1);
-	}
+		return (malloc_error_handler(0, info, philo));
 	i = 0;
 	while (i < n)
 	{
@@ -184,22 +224,15 @@ int	init_philosophers(t_config *config, t_overall_info *info, t_philo **philo)
 		(*philo)[i].config = config;
 		(*philo)[i].info = info;
 		(*philo)[i].last_ate_time = 0;
-//		if (i == 3)
-//			(*philo)[i].last_ate = NULL;
-//		else
-		(*philo)[i].last_ate = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+		(*philo)[i].last_ate = (t_mutex *)malloc(sizeof(t_mutex));
 		if ((*philo)[i].last_ate == NULL)
-		{
-			malloc_error_handler(i, info, philo);
-			return (1);
-		}
-//		pthread_mutex_init((*philo)[i].last_ate, NULL);
+			return (malloc_error_handler(i, info, philo));
 		i++;
 	}
 	return (0);
 }
 
-void	init_mutex(int n, t_overall_info *info, t_philo **philo)
+void	init_mutex(int n, t_overall *info, t_philo **philo)
 {
 	int	i;
 
@@ -229,14 +262,13 @@ bool	check_end(t_philo *philo)
 
 long	get_elapsed_time_in_ms(long start_time)
 {
-	struct timeval	now_time;
+	struct timeval	now;
 	long			elapsed_time;
 
-	gettimeofday(&now_time, NULL);
-	elapsed_time = (now_time.tv_sec * 1e3 + now_time.tv_usec * 1e-3) - start_time;
+	gettimeofday(&now, NULL);
+	elapsed_time = (now.tv_sec * 1e3 + now.tv_usec * 1e-3) - start_time;
 	return (elapsed_time);
 }
-
 
 void	print_status(int status, t_philo *philo)
 {
@@ -273,7 +305,8 @@ void	eating(t_philo *philo)
 	print_status(EATINGN, philo);
 	update_last_ate_time(philo);
 	sleep_start_time = get_elapsed_time_in_ms(philo->config->start_time);
-	while (get_elapsed_time_in_ms(philo->config->start_time) - sleep_start_time < philo->config->time_to_eat)
+	while (get_elapsed_time_in_ms(philo->config->start_time) - sleep_start_time
+		< philo->config->time_to_eat)
 		usleep(500);
 	philo->number_of_meals++;
 }
@@ -284,7 +317,8 @@ void	sleeping(t_philo *philo)
 
 	print_status(SLEEPING, philo);
 	sleep_start_time = get_elapsed_time_in_ms(philo->config->start_time);
-	while (get_elapsed_time_in_ms(philo->config->start_time) - sleep_start_time < philo->config->time_to_sleep)
+	while (get_elapsed_time_in_ms(philo->config->start_time) - sleep_start_time
+		< philo->config->time_to_sleep)
 		usleep(500);
 }
 
@@ -295,11 +329,11 @@ void	thinking(t_philo *philo)
 
 void	routine(void *arg)
 {
-	int	n;
-	t_philo *philo;
+	int		n;
+	t_philo	*philo;
 
 	philo = (t_philo *)arg;
-	n = philo->config->number_of_philosophers;
+	n = philo->config->num_of_philo;
 	if ((philo->id + 1) % 2 == 0)
 		usleep(200);
 	while (!check_end(philo))
@@ -322,37 +356,45 @@ void	routine(void *arg)
 	return ;
 }
 
+int	check_died(int i, t_philo *philo)
+{
+	long	now_time;
+
+	pthread_mutex_lock(philo[i].last_ate);
+	now_time = get_elapsed_time_in_ms(philo->config->start_time);
+	if (now_time - philo[i].last_ate_time >= philo->config->time_to_die)
+	{
+		pthread_mutex_unlock(philo[i].last_ate);
+		pthread_mutex_lock(philo->info->end_flag);
+		philo->info->end_simulation = 1;
+		pthread_mutex_unlock(philo->info->end_flag);
+		print_status(DIE, &philo[i]);
+		return (1);
+	}
+	pthread_mutex_unlock(philo[i].last_ate);
+	return (0);
+}
+
 void	big_brother(void *arg)
 {
-	int	i;
+	int		i;
 	t_philo	*philo;
-	long	now_time;
 
 	philo = (t_philo *)arg;
 	while (1)
 	{	
 		pthread_mutex_lock(philo->info->end_meals);
-		if (philo->info->n_of_finish_eat == philo->config->number_of_philosophers)
+		if (philo->info->n_of_finish_eat == philo->config->num_of_philo)
 		{
 			pthread_mutex_unlock(philo->info->end_meals);
 			return ;
 		}
 		pthread_mutex_unlock(philo->info->end_meals);
 		i = 0;
-		while (i < philo->config->number_of_philosophers)
+		while (i < philo->config->num_of_philo)
 		{
-			pthread_mutex_lock(philo[i].last_ate); //cut func check_died
-			now_time = get_elapsed_time_in_ms(philo->config->start_time);
-			if (now_time - philo[i].last_ate_time >= philo->config->time_to_die)
-			{
-				pthread_mutex_unlock(philo[i].last_ate);
-				pthread_mutex_lock(philo->info->end_flag);
-				philo->info->end_simulation = 1;
-				pthread_mutex_unlock(philo->info->end_flag);
-				print_status(DIE, &philo[i]);
+			if (check_died(i, philo))
 				return ;
-			}
-			pthread_mutex_unlock(philo[i].last_ate); //so_far
 			i++;
 		}
 		usleep(300);
@@ -369,21 +411,21 @@ int	thread_error_handler(t_philo *philo, int flag)
 
 void	thread_processing(int n, t_philo *philo)
 {
-	int	i;
-	int j;
+	int			i;
+	int			j;
 	pthread_t	monitor;
-	int	error_flag;
+	int			error_flag;
 
 	error_flag = 0;
-	i = 0;
-	while (i < n)
+	i = -1;
+	while (++i < n)
 	{
-		if (pthread_create(&philo[i].thread, NULL, (void *)routine, &philo[i]) != 0)
+		if (pthread_create(&philo[i].thread, NULL, (void *)routine, &philo[i])
+			!= 0)
 		{
 			error_flag |= thread_error_handler(&philo[i], PHILO_ERROR);
 			break ;
 		}
-		i++;
 	}
 	if (pthread_create(&monitor, NULL, (void *)big_brother, philo) != 0)
 		error_flag |= thread_error_handler(philo, MONITOR_ERROR);
@@ -396,7 +438,7 @@ void	thread_processing(int n, t_philo *philo)
 		printf("pthread_create_error\n");
 }
 
-void	destroy_mutex(int n, t_overall_info *info, t_philo **philo)
+void	destroy_mutex(int n, t_overall *info, t_philo **philo)
 {
 	int	i;
 
@@ -415,66 +457,27 @@ void	destroy_mutex(int n, t_overall_info *info, t_philo **philo)
 	free(info->end_meals);
 	free(info->get_time_and_print);
 	free(info->end_flag);
-	free(*philo);	
+	free(*philo);
 }
 
 int	main(int argc, char **argv)
 {
-	t_config	config;
-	t_overall_info	info;
-	t_philo		*philo;
-//	pthread_t	monitor;
-//	int	i;
-//	int	debg;
+	t_config		config;
+	t_overall		info;
+	t_philo			*philo;
 	struct timeval	now;
 
 	if (!check_args(argc, argv))
 		return (1);
 	init_config(argv, &config);
-	if (init_overall_info(config.number_of_philosophers, &info) != 0)
+	if (init_overall_info(config.num_of_philo, &info) != 0)
 		return (1);
 	if (init_philosophers(&config, &info, &philo) != 0)
 		return (1);
-	init_mutex(config.number_of_philosophers, &info, &philo);
+	init_mutex(config.num_of_philo, &info, &philo);
 	gettimeofday(&now, NULL);
 	philo->config->start_time = (now.tv_sec * 1e3) + (now.tv_usec * 1e-3);
-	thread_processing(config.number_of_philosophers, philo);
-	
-//	i = 0;
-//	while (i < config.number_of_philosophers)
-//	{
-//		debg = pthread_create(&philo[i].thread, NULL, (void *)routine, &philo[i]);
-//		if (debg != 0)
-//			printf("creat ko\n");
-//		i++;
-//	}
-//	pthread_create(&monitor, NULL, (void *)big_brother, philo);
-//	i = 0;
-//	while (i < config.number_of_philosophers)
-//	{
-//		debg = pthread_join(philo[i].thread, NULL);
-////		if (debg != 0)
-////			printf("join ko\n");
-////		i++;
-//	}
-//	pthread_join(monitor, NULL);
-
-	destroy_mutex(config.number_of_philosophers, &info, &philo);
-//	i = 0;
-//	while (i < config.number_of_philosophers)
-//	{
-//		pthread_mutex_destroy(philo[i].last_ate);
-//		free(philo[i].last_ate);
-//		pthread_mutex_destroy(&(info.fork)[i]);
-//		i++;
-//	}
-//	free(info.fork);
-//	pthread_mutex_destroy(info.end_meals);
-//	pthread_mutex_destroy(info.get_time_and_print);
-//	pthread_mutex_destroy(info.end_flag);
-//	free(info.end_meals);
-//	free(info.get_time_and_print);
-//	free(info.end_flag);
-//	free(philo);
+	thread_processing(config.num_of_philo, philo);
+	destroy_mutex(config.num_of_philo, &info, &philo);
 	return (0);
 }
